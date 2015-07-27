f197ca23f08a35eeecc5346b966df779e2a12926
Demo User <ncmbeledogu@gmail.com>
2015-07-27 11:30:27 -0700

    the submit page now adds words to the datastore

--- our-app/main.py
+++ our-app/main.py
@@ -1,25 +1,38 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import jinja2
+from google.appengine.ext import ndb
+import datetime
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+
+class Word(ndb.Model):
+    location = ndb.StringProperty(required=True)
+    word = ndb.StringProperty(required=True)
+    definition = ndb.TextProperty(required=True)
+    timestamp = ndb.DateTimeProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write("You just added a word.")
+
+class AddWordHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("submit.html")
+        self.response.write(template.render())
+    def post(self):
+        location = self.request.get("location_box")
+        word = self.request.get("word_box")
+        definition = self.request.get("definition_box")
+        added_word = Word(location=location,
+                          word=word,
+                          definition=definition,
+                          timestamp=datetime.datetime.now())
+        added_word.put()
+        self.redirect("/")
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ("/", MainHandler),
+    ("/add", AddWordHandler)
 ], debug=True)
