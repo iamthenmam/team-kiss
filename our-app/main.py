36f108e91a7976f6abaa3440350c1bd07276d7d3
Demo User <demouser@MacBook-Pro.local>
2015-07-27 12:08:11 -0700

    homepagebutton

--- our-app/main.py
+++ our-app/main.py
@@ -1,38 +1,9 @@
-
 import webapp2
-import jinja2
-from google.appengine.ext import ndb
-import datetime
-
-env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
-
-class Word(ndb.Model):
-    location = ndb.StringProperty(required=True)
-    word = ndb.StringProperty(required=True)
-    definition = ndb.TextProperty(required=True)
-    timestamp = ndb.DateTimeProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("You just added a word.")
-
-class AddWordHandler(webapp2.RequestHandler):
-    def get(self):
-        template = env.get_template("submit.html")
-        self.response.write(template.render())
-    def post(self):
-        location = self.request.get("location_box")
-        word = self.request.get("word_box")
-        definition = self.request.get("definition_box")
-        added_word = Word(location=location,
-                          word=word,
-                          definition=definition,
-                          timestamp=datetime.datetime.now())
-        added_word.put()
-        self.redirect("/")
-
+        self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ("/", MainHandler),
-    ("/add", AddWordHandler)
+    ('/', MainHandler)
 ], debug=True)
