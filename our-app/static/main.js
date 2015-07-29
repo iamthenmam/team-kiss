f627dbc6083e442947a53a9180e9ff87b1236516
Nma Mbeledogu <ncmbeledogu@gmail.com>
2015-07-28 17:19:43 -0700

    trying to geocode addresses

--- our-app/static/main.js
+++ our-app/static/main.js
@@ -4,18 +4,6 @@
 // $("#search-button").click(function() {
 //   $("#results").show()
 
-function initialize() {
-  var mapCanvas = document.getElementById("map-canvas");
-
-  var mapOptions = {
-    center: new google.maps.LatLng(39.8282, -98.5795),
-    zoom: 4,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  backgroundMap = new google.maps.Map(mapCanvas, mapOptions);
-
-}
 
 function loadScript() {
   var script = document.createElement("script");
@@ -27,3 +15,38 @@ function loadScript() {
 }
 
 window.onload = loadScript;
+
+var geocoder
+var backgroundMap
+
+function initialize() {
+  var mapCanvas = document.getElementById("map-canvas");
+
+  var mapOptions = {
+    center: new google.maps.LatLng(39.8282, -98.5795),
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  backgroundMap = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+function codeAddress() {
+  var converted_location
+  var geocoder = new google.maps.Geocoder();
+  var address = document.getElementById("uncoded_location").value;
+  geocoder.geocode( { "address": address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var converted_location = results[0].geometry.location
+      map.setCenter(converted_location);
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+  function setOutput(itsId, itsValue) {
+    document.getElementById(itsId).value = itsValue;
+  };
+  setOutput(uncoded_location, converted_location);
+}
+
+////////////////////////////////////////////////////////////////////////////////
