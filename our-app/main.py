e5398c93f8fccce34efcc1bae9583d8331cbd400
Demo User <demouser@MacBook-Pro.local>
2015-07-30 18:11:51 -0700

    slap a word on the map puts the word into the add page form

--- our-app/main.py
+++ our-app/main.py
@@ -109,7 +109,8 @@ class MainHandler(webapp2.RequestHandler):
 class AddWordHandler(webapp2.RequestHandler):
     def get(self):
         template = env.get_template("submit.html")
-        self.response.write(template.render())
+        variables = {'added_word': self.request.get('added_word')}
+        self.response.write(template.render(variables))
     def post(self):
         location = self.request.get("location_box")
         safe_location = location.replace(" ", "+")
